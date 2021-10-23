acb4b8d028c6fc4e4708d2e079d41bc5af7dc8c2
kimself <ksy7678@gmail.com>
2021-10-23 21:29:12 +0900

    Add code faceAuth

--- 2.Mindtree/faceRecognitionExample.py
+++ 2.Mindtree/faceRecognitionExample.py
@@ -1,11 +1,33 @@
 from datetime import datetime
 import cv2
 import copy
+import dlib
+
+faceDetectorOption = 'cv2' # cv2, dlib
+imageOption = 'cam' # img, cam
 
 opt_cv2Font = cv2.FONT_HERSHEY_SIMPLEX
 opt_cv2FaceBoxColor = (0, 255, 0)   # Blue, Green, Red
 detector_cv2_face = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 detector_cv2_eye = cv2.CascadeClassifier('haarcascade_eye.xml')
+detector_dlib_face = dlib.get_frontal_face_detector()
+
+
+def detectFaceByDLib(frame):
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = detector_dlib_face(gray, 1)
+
+    for f in faces:
+        x1 = f.left()
+        y1 = f.top()
+        x2 = f.right()
+        y2 = f.bottom()
+
+        cv2.rectangle(frame, (x1, y1), (x2, y2), opt_cv2FaceBoxColor, 3, 4, 0)
+        cv2.putText(frame, 'Detected Face', (x1 - 5, y1 - 5), opt_cv2Font, 0.9, (255, 255, 0), 2)
+
+    return frame
+
 
 
 def detectFaceByOpenCV(frame):
@@ -27,6 +49,7 @@ def detectFaceByOpenCV(frame):
         eyes = detector_cv2_eye.detectMultiScale(image=face_gray, scaleFactor=1.03, minNeighbors=7)
         for (ex, ey, ew, eh) in eyes:
             existEye = True
+            cv2.rectangle(face_color, (ex, ey), (ex+ew, ey+eh), (255, 0, 0), 2)
 
         if existEye:
             filterFaces.append([x1, y1, x2, y2])
@@ -42,23 +65,44 @@ def detectFaceByOpenCV(frame):
 
 
 def getReducedFaces(faces):
-    # 1. 전체를 돌면서, 합쳐야 될 것과 아닌것을 구분한다
-    # 2. 합쳐야 될 필요가 없는 것은, 최종 리스트에 추가 시킨다
-    # 3. 합쳐야 될 필요가 있는 것은,
-
-
     filteredFaces = copy.deepcopy(faces)
-    while existDuplicate(filteredFaces) == True:
-        filteredFaces = reduceFaces(copy.deepcopy(filteredFaces))
+    dupCnt = 0
 
+    while True:
+        i, j = existDuplicate(filteredFaces)
+        if i==-1 and j==-1:
+            break
+
+        dupCnt = dupCnt + 1
+        print("exist duplicate - {0}".format(dupCnt))
+
+        filteredFaces = reduceFaces(copy.deepcopy(filteredFaces), i, j)
 
     return filteredFaces
 
-def reduceFaces(faces):
-    return faces
+def reduceFaces(faces, idx_1, idx_2):
+    reducedFaces = []
+
+    for i in range(len(faces)):
+        if i==idx_1 or i==idx_2:
+            continue
+
+        reducedFaces.append(faces[i])
+
+
+    new_x1 = min(faces[idx_1][0], faces[idx_2][0])
+    new_y1 = min(faces[idx_1][1], faces[idx_2][1])
+    new_x2 = max(faces[idx_1][2], faces[idx_2][2])
+    new_y2 = max(faces[idx_1][3], faces[idx_2][3])
+    reducedFaces.append([new_x1, new_y1, new_x2, new_y2])
+
+    return copy.deepcopy(reducedFaces)
 
 def existDuplicate(faces):
     ratioLimit = 0.4
+
+    dupIdx_1 = -1
+    dupIdx_2 = -1
 
     for i in range(len(faces)-1):
         for j in range(i + 1, len(faces)):
@@ -73,12 +117,18 @@ def existDuplicate(faces):
                 j_area = (faces[j][2] - faces[j][0]) * (faces[j][3] - faces[j][1])
 
                 if (dupArea / min(i_area, j_area)) > ratioLimit:
-                    print("exist duplicate", dupArea / min(i_area, j_area))
-                    print(min(faces[i][0], faces[j][0]), min(faces[i][1], faces[j][1]))
-                    print(min(faces[i][2], faces[j][2]), min(faces[i][3], faces[j][3]))
+                    # print("exist duplicate", dupArea / min(i_area, j_area))
+                    # print(min(faces[i][0], faces[j][0]), min(faces[i][1], faces[j][1]))
+                    # print(min(faces[i][2], faces[j][2]), min(faces[i][3], faces[j][3]))
+                    dupIdx_1 = i
+                    dupIdx_2 = j
+                    break
+
+        if dupIdx_1 >= 0:
+            break
 
 
-    return False
+    return dupIdx_1, dupIdx_2
 
 
 
@@ -86,9 +136,12 @@ def existDuplicate(faces):
 def startImage():
     frame = cv2.imread("bts_sample_1.jpeg")
 
-    processedFrame = detectFaceByOpenCV(frame)
-
-
+    if faceDetectorOption == 'cv2':
+        processedFrame = detectFaceByOpenCV(frame)
+    elif faceDetectorOption == 'dlib':
+        processedFrame = detectFaceByDLib(frame)
+    else:
+        processedFrame = frame
 
 
     cv2.imshow("test", processedFrame)
@@ -105,8 +158,12 @@ def startWebCam():
         if not ret:
             return
 
-
-        processedFrame = detectFaceByOpenCV(frame)
+        if faceDetectorOption == 'cv2':
+            processedFrame = detectFaceByOpenCV(frame)
+        elif faceDetectorOption == 'dlib':
+            processedFrame = detectFaceByDLib(frame)
+        else:
+            processedFrame = frame
 
 
 
@@ -130,7 +187,9 @@ def getTimeStr():
 if __name__ == "__main__":
     print(getTimeStr(), "Start - Face Recognition Example")
 
-    startImage()
-    # startWebCam()
+    if imageOption == 'img':
+        startImage()
+    elif imageOption == 'cam':
+        startWebCam()
 
     print(getTimeStr(), "Finish - Face Recognition Example")
